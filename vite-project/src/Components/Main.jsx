ff64761e22e54eac180da4680d60286f8bf4f7bc
Evgeniy <eugeniymokrushin@gmail.com>
2022-08-24 20:18:21 +0300

    done++

--- vite-project/src/Components/Main.jsx
+++ vite-project/src/Components/Main.jsx
@@ -1,31 +1,70 @@
 import React from "react";
-import memesData from '../memesData.js'
+
 
 
 export default function Main() {
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData.data.memes)
+    const [allMemes, setAllMemes] = React.useState([])
     const [meme, setMeme] = React.useState(
         {
-            imgMeme: "",
+            memeId: 0,
+            imgMeme: "http://i.imgflip.com/1bij.jpg",
             topText: "",
             bottomText: ""
         }
     )
 
+    React.useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+        console.log('I run')
+    }, []);
+
     function getMemeImage() {
-        const memesArray = allMemeImages
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMeme(prevState => ({ ...prevState, imgMeme: memesArray[randomNumber].url }))
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imgMeme: url
+        }))
+        
+    }
+
+    function handleChange(e) {
+        const { value, name } = e.target
+        setMeme(prevState => {
+            return (
+                {
+                    ...prevState,
+                    [name]: value
+                }
+            )
+        })
 
     }
+
+
 
 
     return (
         <main>
             <div className="form">
                 <div className="inputs">
-                    <input type="text" className="upper-text" placeholder="Top text" />
-                    <input type="text" className="lower-text" placeholder="Bottom text" />
+                    <input type="text"
+                        value={meme.topText}
+                        className="upper-text"
+                        placeholder="Top text"
+                        name="topText"
+                        onChange={handleChange}
+                    />
+
+                    <input type="text"
+                        className="lower-text"
+                        placeholder="Bottom text"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
                 </div>
                 <button type="submit" className="create-btn" onClick={getMemeImage}>
                     Get a new meme image
@@ -33,6 +72,8 @@ export default function Main() {
             </div>
             <div className="generated-meme">
                 {meme.imgMeme != "" && <img src={meme.imgMeme} alt="Meme image" className="meme--photo" />}
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
